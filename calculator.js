87bcbdb3ffc33de29f3d2db30eeca2891cb36004
Dillon Huff <dillonhuff@gmail.com>
2020-07-27 23:44:46 -0600

    added skeleton of the design

--- calculator.js
+++ calculator.js
@@ -1,7 +1,17 @@
+class Quantity {
+
+  constructor(name, magnitude, unit) {
+    this.name = name;
+    this.magnitude = magnitude;
+    this.unit = unit;
+  }
+
+}
+
 $('input').on("keypress",function (e) {
   if (e.which == 13) {
     $('#result').val(parseInt($("#amount").val(), 10)*  15);
-    $("#menu").append('<li> ' + $("#amount").val() + ' </li>');
+    $("#Quantities").append('<li> ' + $("#amount").val() + ' </li>');
   }
 });
 
