33a7d7eb8e06624d38965c0c8c17e8abbba8b6f4
Dillon Huff <dillonhuff@gmail.com>
2020-07-27 23:30:24 -0600

    now adding data on enter

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
-$('input').on("keyup",function () {
-  $('#result').val(parseInt($("#amount").val(), 10)*  15);
-  $("#menu").append('<li><a href="#">' + $("#amount").val() + '</a></li>');
+$('input').on("keypress",function (e) {
+  if (e.which == 13) {
+    $('#result').val(parseInt($("#amount").val(), 10)*  15);
+    $("#menu").append('<li> ' + $("#amount").val() + ' </li>');
+  }
 });
 
 
